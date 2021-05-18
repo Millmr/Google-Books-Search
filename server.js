e7ae5650580cec2b36be7a575e4fc810708efc4c
Matthew Miller <matt.r.miller09@gmail.com>
2021-05-18 18:44:02 -0500

    changed mongoose.connect

--- server.js
+++ server.js
@@ -15,13 +15,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/google-books-search',
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false
-    }
+    process.env.MONGODB_URI || "mongodb://localhost/googlebooks"
 );
 
 
